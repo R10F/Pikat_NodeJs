23d713c8f41634cb4f44ea5180f2f4869c68278c
R10F <farrellriofa@gmail.com>
2023-06-10 20:33:14 +0700

    Jawaban hsil diskusi

--- controllers/latihan.js
+++ controllers/latihan.js
@@ -1,13 +1,21 @@
 let kumpulan_data = [];
 
 exports.getData = (req, res) => {
-  res.status(200).json({ data: kumpulan_data });
+  if (kumpulan_data.length === 0) {
+    res.status(404).json({ message: "Data still empty" });
+  } else {
+    res.status(200).json({ data: kumpulan_data });
+  }
 };
 
 exports.addData = (req, res) => {
-  let id = req.body.id;
+  let id = kumpulan_data.length + 1;
   let nama = req.body.nama;
-  kumpulan_data.push({ id, nama });
+  let nim = req.body.nim;
+  let kelas = req.body.kelas;
+  let alamat = req.body.alamat;
+
+  kumpulan_data.push({ id, nama, nim, kelas, alamat }); //
   res.status(201).json({ message: "Data Created" });
 };
 
@@ -19,7 +27,17 @@ exports.deleteData = (req, res) => {
   let id = Number(req.params.id);
   let filteredData = kumpulan_data.filter((data) => data.id !== id);
 
-  kumpulan_data = filteredData;
-
-  res.status(202).send({ message: "Data Deleted" });
+  // for (let i = 0; i <= kumpulan_data.length; i++) {
+  //   if (kumpulan_data[i]["id"] === id) {
+  //     res.status(202).json({ message: `Data With Id ${id} deleted` });
+  //     kumpulan_data = filteredData;
+  //   }
+  // }
+  kumpulan_data.forEach((data) => {
+    if (id === data.id) {
+      res.status(202).json({ message: `Data With Id ${id} deleted` });
+      kumpulan_data = filteredData;
+    }
+  });
+  res.status(500).json({ message: `Data with id ${id} not found` });
 };
